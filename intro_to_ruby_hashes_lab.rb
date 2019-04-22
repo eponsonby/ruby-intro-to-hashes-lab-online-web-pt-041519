10b3b709fce6630a3e94809d62f88391732c4987
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-22 22:12:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,13 +22,16 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {
+  	monopoly = {
 	  railroads:{
 	    pieces: 4,
 	    names: {},
-	    rent_in_dollars: {}
+	    rent_in_dollars: {
+	      one_piece_owned: 25
+	    }
 	  }
 	}
+
 
 end
 
